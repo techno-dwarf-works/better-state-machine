e7058b5534ed386ec668cc314a24b68e38c089b4
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-05-21 02:17:05 +0200

    Fix 0.1.10

--- Assets/BetterStateMachine/Runtime/Sequences/Sequence.cs
+++ Assets/BetterStateMachine/Runtime/Sequences/Sequence.cs
@@ -8,8 +8,8 @@ namespace Better.StateMachine.Runtime.Sequences
     [Serializable]
     public abstract class Sequence<TState> where TState : BaseState
     {
-        protected internal abstract Task PreProcessingAsync(TState fromState, TState toState, CancellationToken cancellationToken);
+        protected internal abstract void OnPreProcessing(TState fromState, TState toState);
         protected internal abstract Task<bool> ProcessingAsync(TState fromState, TState toState, CancellationToken cancellationToken);
-        protected internal abstract Task PostProcessingAsync(TState fromState, TState toState, CancellationToken cancellationToken);
+        protected internal abstract void OnPostProcessing(TState fromState, TState toState);
     }
 }
